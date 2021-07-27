60a2a4f52a49dbe2fa1875da42d4befa29173a5f
feadonai123 <felipe.2002@alunos.utfpr.edu.br>
2021-07-27 14:23:37 -0300

    Next project for site

--- app/pages/home.js
+++ app/pages/home.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Home
+          Minha Home
         </h1>
       </main>
     </div>
